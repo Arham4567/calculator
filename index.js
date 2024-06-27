9587a405a2d2a684cf58e3128d847622dbaddab0
Arham Afzal <wwwarhamafzal682@gmail.com>
2024-06-27 17:49:50 +0500

    Simple Calculator

---
+++ index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const a = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter your first number",
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter your second number",
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Enter your operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+function sum(num1, num2) {
+    const result = num1 + num2;
+    console.log(`sum of ${num1} and ${num2} = ${result}`);
+}
+function subtract(num1, num2) {
+    const result = num1 - num2;
+    console.log(`subtraction of ${num1} and ${num2} = ${result}`);
+}
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    console.log(`multiplication of ${num1} and ${num2} = ${result}`);
+}
+function divide(num1, num2) {
+    const result = num1 / num2;
+    console.log(`division of ${num1} and ${num2} = ${result}`);
+}
+if (a.operator == "+") {
+    sum(a.num1, a.num2);
+}
+else if (a.operator == "-") {
+    subtract(a.num1, a.num2);
+}
+else if (a.operator == "*") {
+    multiply(a.num1, a.num2);
+}
+else if (a.operator == "/") {
+    divide(a.num1, a.num2);
+}
